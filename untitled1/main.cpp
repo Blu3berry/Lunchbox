3132a6ccdc7450bae569ce20fd4dc0725953a56c
Blu3berry <43879315+Blu3berry@users.noreply.github.com>
2018-10-05 15:47:41 +0200

    das hier
    Hier

---
+++ untitled1/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <rpcndr.h>
+
+using namespace std;
+
+int main() {
+    int foo [6] = { 15,-17, 20, 5, 40, -10 };
+    int n =sizeof(foo)/sizeof(int);
+    int max = 0;
+    for (int i=0; i<n; i++ ) {
+        for ( int j =i; j<n; j++ ) {
+            int sum = 0;
+            for ( int k=i; k<j; k++ ) {
+                sum += foo[k];
+                if(sum>max){
+                    max=sum;
+                }
+            }
+        }
+    }
+    cout <<max;
+    return 0;
+}
+vector simplify(int*p,int length){
+    boolean neg=0;
+    boolean pos=1;
+    boolean prev=0;
+    int sum=0;
+    vector<int> bar;
+    for(int i=0;i<length;i++){
+        if (*(p+i*sizeof(int))<0){
+            neg = 1;
+            pos = 0;
+        }if(*(p+i*sizeof(int))>0){
+            neg=0;
+            pos=1;
+        }
+        if(i==0){
+            sum =*(p+sizeof(int));
+            prev= sum < 0 ? 0 : 1;
+        }
+        if((!prev)&&neg){
+            sum +=*(p+sizeof(i));
+        }else if((prev&&pos)){
+                sum+=*(p+sizeof(i));
+        }else if(prev&&neg){
+            prev=0;
+            bar.push_back(sum);
+            sum =*(p+sizeof(int));
+        }else if(!prev&&pos){
+            prev=1;
+            bar.push_back(sum);
+            sum=*(p+sizeof(int));
+        }
+
+    }
+    bar.push_back(sum);
+
+    return bar;
+
+}
